5813017d816935aa7821d7f6f3f33563655978f0
krishna-garikapati <w0502117@campus.nscc.ca>
2024-11-19 14:57:30 -0400

    update_ready for marking

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -9,11 +9,13 @@ def Replace():
     phrase=""
     letter=""
     #i want run the loop and ask for inputs until phrase = quit, the exit loop.
-    while phrase!="quit":
+    while True:
 
         #lets give input values
         phrase=input("Type a phrase (or type quit to exit program):")
-        letter=input("Type a Comma-separated list of letters to redact:").lower()
+        if phrase.lower()=="quit":
+            break
+        letter=input("Type a Comma-separated list of letters to redact:")
 
         #I want values given separated by comma as different values. So using split function.
         letterstoRedact=letter.split(",")
@@ -21,8 +23,8 @@ def Replace():
 
         #now replace each value from input received in the phrase input received.
         for value in letterstoRedact:
-            phrase=phrase.replace(value,'_')
-
+            phrase=phrase.replace(value.lower(),'_')
+            phrase=phrase.replace(value.upper(),'_')
             #to count number of letters replaced or redacted
             count=phrase.count('_')
         print("Number of letters redacted: {0}".format(count))

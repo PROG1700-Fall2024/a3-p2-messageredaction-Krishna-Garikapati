827a677d75d46f9fec03bc79679abfec943646fb
krishna-garikapati <w0502117@campus.nscc.ca>
2024-11-19 13:02:54 -0400

    Ready for marking

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -2,19 +2,42 @@
 #Description:   Design and write a program that counts and removes all desired letters from 
 #               any user-entered sentence or phrase.
 
-#Student #:     
-#Student Name:  
+#Student #:  W0502117 
+#Student Name: Krishna Priyanka, Garikapati 
+
+def Replace():
+    phrase=""
+    letter=""
+    #i want run the loop and ask for inputs until phrase = quit, the exit loop.
+    while phrase!="quit":
+
+        #lets give input values
+        phrase=input("Type a phrase (or type quit to exit program):")
+        letter=input("Type a Comma-separated list of letters to redact:").lower()
+
+        #I want values given separated by comma as different values. So using split function.
+        letterstoRedact=letter.split(",")
+        #print(letterstoRedact)
+
+        #now replace each value from input received in the phrase input received.
+        for value in letterstoRedact:
+            phrase=phrase.replace(value,'_')
+
+            #to count number of letters replaced or redacted
+            count=phrase.count('_')
+        print("Number of letters redacted: {0}".format(count))
+        print("Redacted phase: {0}".format(phrase))
+        
+
+
+
+
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
-
-
-
-
-
-
-
+    Replace()
+    
     # YOUR CODE ENDS HERE
 
 main()
